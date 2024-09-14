3f4d07c19c9ed8c47db5c3e354f056533112ee1e
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-09-15 00:12:45 +0600

    Cycle Detection using BFS Done

---
+++ cycle_detection_graph_algorithm/cycle_detection_using_bfs.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> ad[N];
+bool vis[N];
+int parentArray[N];
+bool ans;
+
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+    vis[s] = true;
+
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+
+        // cout << parent << " ";
+
+        for (int child : ad[parent])
+        {
+            if (vis[child] == true && parentArray[parent] != child)
+            {
+                ans = true;
+            }
+            if (!vis[child])
+            {
+                vis[child] = true;
+                parentArray[child] = parent;
+                q.push(child);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        ad[a].push_back(b);
+        ad[b].push_back(a);
+    }
+
+    memset(vis, false, sizeof(vis));
+    memset(parentArray, -1, sizeof(parentArray));
+    ans = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            bfs(i);
+        }
+    }
+
+    if (ans)
+        cout << "Cycle Found" << endl;
+    else
+        cout << "Cycle Not Found" << endl;
+
+    return 0;
+}
