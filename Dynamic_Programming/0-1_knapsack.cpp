33ffe2ae6e0de20de9e62f62ac8c34dfd7dd9ff0
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-10-12 23:31:06 +0600

    0-1 Knapsack Problem Solved using DP Memoization

--- Dynamic_Programming/0-1_knapsack.cpp
+++ Dynamic_Programming/0-1_knapsack.cpp
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int maxN = 1000;
+const int maxW = 1000;
+int dp[maxN][maxW];
+
 int knapsack(int n, int weight[], int value[], int W)
 {
-    if (n < 0 || W == 0)
+    if (n == 0 || W == 0)
         return 0;
-    if (weight[n] <= W)
+
+    if (dp[n][W] != -1)
+        return dp[n][W];
+
+    if (weight[n - 1] <= W)
     {
         // duita option
         // niye dekho, na niye dekho
-        int op1 = knapsack(n - 1, weight, value, W - weight[n]) + value[n];
+        int op1 = knapsack(n - 1, weight, value, W - weight[n - 1]) + value[n - 1];
         int op2 = knapsack(n - 1, weight, value, W);
-        return max(op1, op2);
+        return dp[n][W] = max(op1, op2);
     }
     else
     {
@@ -40,7 +48,15 @@ int main()
     int W;
     cin >> W;
 
-    cout << knapsack(n - 1, weight, value, W) << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= W; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
+    cout << knapsack(n, weight, value, W) << endl;
 
     return 0;
 }
